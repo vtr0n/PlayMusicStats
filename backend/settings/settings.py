baf8f722ace17ffbbf6759d4fba80cbede24c895
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-09-03 13:12:47 +0300

    django source moved to backend directory

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'settings.urls'
+ROOT_URLCONF = 'backend.settings.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'settings.wsgi.application'
+WSGI_APPLICATION = 'backend.settings.wsgi.application'
 
 
 # Database
