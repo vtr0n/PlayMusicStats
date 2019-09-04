b478b2889523ec81d7f0dce5e7a74adb06994e22
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-09-04 12:00:28 +0300

    cli command which saved gm stats added

---
+++ backend/stats/management/commands/update_users_stats.py
@@ -0,0 +1,38 @@
+import pickle, codecs
+from django.core.management.base import BaseCommand
+from backend.stats.models import PlayMusicStats
+from backend.users.models import UserSettings
+from gmusicapi import Mobileclient
+
+
+class Command(BaseCommand):
+    help = 'Get gm stats and save it to mongo'
+
+    def handle(self, *args, **options):
+        gm = Mobileclient()
+        users = UserSettings.objects.all()
+
+        for user in users:
+            if user.credential_is_valid is False:
+                continue
+
+            try:
+                user_credential_base64 = user.credential
+                user_credential = codecs.decode(user_credential_base64.encode(), "base64")
+                credential = pickle.loads(user_credential)
+
+                # TODO think about google apis problem
+                gm.oauth_login(user.current_device, credential)
+                library = gm.get_all_songs()
+
+                new_stats = PlayMusicStats()
+                new_stats.user = user.user
+                new_stats.stats = library
+                new_stats.save()
+
+                self.stdout.write(self.style.SUCCESS('Stats for ' + str(user.user) + ' saved'))
+            except:
+                user.credential_is_valid = False
+                user.save()
+                self.stdout.write(self.style.ERROR('Credential for ' + str(user.user) + ' is invalid'))
+                continue
