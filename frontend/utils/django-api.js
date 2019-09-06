bf6f5655e46e0c774dc0de7a27e2ac22ec8b2d20
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-09-06 19:57:34 +0300

    added console.log

--- frontend/utils/django-api.js
+++ frontend/utils/django-api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {getAccessToken} from './auth';
 
+console.log(process.env);
 const BASE_URL = process.env.VUE_API_BASE_URL;
 
 export {getAuthUrl, confirmCode, getUserInfo, getStats};
