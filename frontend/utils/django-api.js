2b9c8dca3d46387056fc009390187bdcd69dfa7f
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-09-06 17:20:23 +0300

    gitlab-ci testing

--- frontend/utils/django-api.js
+++ frontend/utils/django-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {getAccessToken} from './auth';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = process.env.VUE_API_BASE_URL;
 
 export {getAuthUrl, confirmCode, getUserInfo, getStats};
 
