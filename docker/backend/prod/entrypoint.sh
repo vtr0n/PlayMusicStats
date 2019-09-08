c89b3c763b53181b976e4c10d25ea4f22d44e2e2
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-09-08 11:46:45 +0300

    temporary fix dns problem

--- docker/backend/prod/entrypoint.sh
+++ docker/backend/prod/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 10
+echo 52.58.196.119 dev-z912l6w0.eu.auth0.com > /etc/hosts
 ./manage.py migrate --no-input
 ./manage.py migrate --database=mongo --no-input
 ./manage.py runserver 0.0.0.0:8000
